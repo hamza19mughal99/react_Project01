7ceefc76b2d86d3fc3234645e9c23d1270dba4b2
Hamza Ahmed Mughal <hamza19mughal99@gmail.com>
2020-06-03 10:19:00 +0500

    updated app.js
    updated app.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br/>
+          Hello from Hamza.
         </p>
         <a
           className="App-link"
